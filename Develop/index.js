e2e3495222abbb0c1ae249859ee7114e040fe956
Brittanie Barrett <britt.barrett302@gmail.com>
2020-05-22 13:25:11 -0700

    finished hw code

--- Develop/index.js
+++ Develop/index.js
@@ -1,39 +1,114 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const axios = require("axios");
 const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown");
 const writeFileAsync = util.promisify(fs.writeFile);
-
 const questions = [
   {
     type: "input",
-    name: "name",
-    message: "What is your username?",
+    name: "title",
+    message: "What is your project title?",
   },
   {
     type: "input",
-    name: "location",
-    message: "Where are you from?",
+    name: "description",
+    message: "What is your project's description?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What do you need to install?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Do you have any examples for use?",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "Do you have any liscence restrictions?",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Who are the contributors?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message:
+      "Do you have any tests for your application? If so provide how to run it",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Credits?",
   },
 ];
-
 function writeToFile(fileName, data) {
   console.log("should write into file");
   return writeFileAsync(fileName, data);
 }
-
 function init() {
-  inquirer.prompt(questions).then(function ({ username, location }) {
-    console.log(location);
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    console.log(queryUrl);
-    axios.get(queryUrl).then(function (res) {
-      const repoNames = res.data.avatar_url;
+  inquirer
+    .prompt(questions)
+    .then(function ({
+      title,
+      description,
+      installation,
+      usage,
+      license,
+      contribution,
+      tests,
+      username,
+      email,
+      credits,
+    }) {
+      //console.log(location);
+      const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+      const userInputObj = {
+        username: username,
+        queryUrl: queryUrl,
+        title: title,
+        description: description,
+        installation: installation,
+        usage: usage,
+        license: license,
+        contribution: contribution,
+        tests: tests,
+        email: email,
+        credits: credits,
+      };
+      return userInputObj;
+    })
+    .then(function (userInputObj) {
+      axios
+        .get(userInputObj.queryUrl)
+        .then(function (res) {
+          // const githubIcon = res.data[0].owner.avatar_url
+          // console.log(githubIcon)
+          userInputObj.githubIcon = res.data[0].owner.avatar_url;
+          //console.log(userInputObj)
+          return userInputObj;
+        })
+        .then(function (userInputObj) {
+          console.log(userInputObj);
+          const html = generateMarkdown(userInputObj);
+          console.log(html);
+          writeToFile("Readme.md", html);
+        });
     });
-    // const html = generateMarkdown(answers, "brittbarrett");
-    // console.log(html);
-    // writeToFile("Readme.md", html);
-  });
 }
-
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,29 @@
-function generateMarkdown(data, githubObj) {
+function generateMarkdown(userInputObj) {
   return `
-# ${data.name}
-
+# Github Account: ${userInputObj.username}
+# Email: ${userInputObj.email}
+![Image of Github User](${userInputObj.githubIcon})
+# ${userInputObj.title}
+## Description 
+${userInputObj.description}
+## Table of Contents (Optional)
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+## Installation
+ ${userInputObj.installation}
+## Usage 
+ ${userInputObj.usage}
+## Credits
+ ${userInputObj.credits}
+## License
+ ${userInputObj.license}
+ ## Contributing
+ ${userInputObj.contribution}
+## Tests
+ ${userInputObj.tests}
 `;
 }
-
 module.exports = generateMarkdown;
